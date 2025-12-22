788fe284747f9a0264ced5f91a997ba1c151d800
SimonDingemans <simon.dingemans@gmail.com>
2025-12-22 10:10:58 +0100

    card structure

---
+++ src/domain/schema.rs
@@ -0,0 +1,67 @@
+use serde::{Deserialize, Serialize};
+use strum::{EnumCount, FromRepr, EnumIter};
+
+pub type BlueprintId = String;
+pub type CardValue = u8;
+pub type TurnValue = u8;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CardBlueprint {
+    pub id: BlueprintId,
+    pub name: String,
+    pub description: String,
+    pub rarity: Rarity,
+    pub card_type: CardType,
+    pub cost: Vec<ManaCost>,
+    pub keywords: Vec<Keyword>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Rarity {
+    Common,
+    Uncommon,
+    Rare,
+    Epic,
+    Legendary,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum CardType {
+    Creature { power: CardValue, health: CardValue },
+    Spell,
+    Artifact,
+    Land { resource: Environment },
+}
+
+#[derive(
+    Debug, Clone, Serialize, Deserialize, Copy,
+    EnumCount, FromRepr, EnumIter 
+)]
+#[repr(u8)]
+pub enum Environment {
+    Forest,
+    Mountain,
+    Swamp,
+    Plains,
+    Island,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum ManaCost {
+    Specific(Environment, CardValue),
+    Neutral(CardValue),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Keyword {
+    Intercept,
+    Stealth { duration: Duration },
+    Crush,
+    Aerial
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Duration {
+    Turns(TurnValue),
+    Permanent,
+}
